c64b10c65ddc9d870916388be01fd87c2216b22a
Hernan <156428836+HernanMA@users.noreply.github.com>
2024-09-16 07:29:39 -0500

    feat: :tada: first project created!

---
+++ Dia1SpringBoot/src/main/java/com/mycompany/dia1springboot/Dia1SpringBoot.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dia1springboot;
+
+/**
+ *
+ * @author hernan
+ */
+public class Dia1SpringBoot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
