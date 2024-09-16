c7147e9a5e21b967ea18c93914acd5cb9f343415
Hernan <hernanariza69@gmail.com>
2024-09-16 08:48:18 -0500

    feat: :wrench:

---
+++ Dia1Spring/src/main/java/com/example/Dia1Spring/Dia1SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dia1Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dia1SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dia1SpringApplication.class, args);
+	}
+
+}
