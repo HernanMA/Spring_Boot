c63d81cb7b06d12deb4f89d8f619c15b448dab00
Hernan <hernanariza69@gmail.com>
2024-09-19 00:23:13 -0500

    feat :sparkles: Connection and Model

--- SpringDia1/src/main/java/com/SpringMVC/SpringDia1/Model/Producto.java
+++ SpringDia1/src/main/java/com/SpringMVC/SpringDia1/Model/Producto.java
@@ -5,20 +5,26 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "producto")
-
-public class Productos {
+@Table(name = "productos")
+public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String descripcion;
-    private String precio;
+    private BigDecimal precio;
     private int cantidad_en_stock;
+    @CreationTimestamp
+    private LocalDateTime dia_pedido;
+    private String fotoProducto;
 }
