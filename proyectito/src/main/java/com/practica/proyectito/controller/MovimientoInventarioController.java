348e6ec44b9ab16e574b79e29123c3a062e8ac76
Hernan <hernanariza69@gmail.com>
2024-09-28 10:37:30 -0500

    ✨movimientos de inventario With html done

--- proyectito/src/main/java/com/practica/proyectito/controller/MovimientoInventarioController.java
+++ proyectito/src/main/java/com/practica/proyectito/controller/MovimientoInventarioController.java
@@ -1,4 +1,28 @@
 package com.practica.proyectito.controller;
 
+import com.practica.proyectito.dto.MovimientosInventarioDto;
+import com.practica.proyectito.service.Movimientos_InventarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class MovimientoInventarioController {
+
+    private final Movimientos_InventarioService movimientosInventarioService;
+
+    @Autowired
+    public MovimientoInventarioController(Movimientos_InventarioService movimientosInventarioService) {
+        this.movimientosInventarioService = movimientosInventarioService;
+    }
+
+    @GetMapping("/movimientos")
+    public String listMovimientos(Model model) {
+        List<MovimientosInventarioDto> movimientosInventario = movimientosInventarioService.findAllMovimientosInventario();
+        model.addAttribute("movimientos", movimientosInventario);
+        return "movimientos-list";
+    }
 }
