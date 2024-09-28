e9e52d2c3b723fb98598ca8e4fca82f0b0a25342
Hernan <hernanariza69@gmail.com>
2024-09-28 10:37:06 -0500

    ✨Proveedores With html done

--- proyectito/src/main/java/com/practica/proyectito/controller/ProveedoresController.java
+++ proyectito/src/main/java/com/practica/proyectito/controller/ProveedoresController.java
@@ -1,4 +1,32 @@
 package com.practica.proyectito.controller;
 
+import com.practica.proyectito.dto.ProveedoresDto;
+import com.practica.proyectito.service.ProveedoresServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class ProveedoresController {
+
+    private final ProveedoresServices proveedoresServices;
+
+    @Autowired
+    public ProveedoresController(ProveedoresServices proveedoresServices) {
+        this.proveedoresServices = proveedoresServices;
+    }
+
+    @GetMapping("/proveedores")
+    public String listProveedores(Model model) {
+        List<ProveedoresDto> proveedores = proveedoresServices.findAllProveedores();
+        model.addAttribute("proveedores", proveedores);
+        return "proveedores-list";
+    }
+
+
+
 }
