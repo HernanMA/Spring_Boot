613cd179aa062d77c969aa8e5f464e4fbd780625
Hernan <hernanariza69@gmail.com>
2024-09-30 17:20:26 -0500

    ✨MONTADO EN EL SERVIDORRRR!!!!

--- proyectito/src/main/java/com/practica/proyectito/ProyectitoApplication.java
+++ proyectito/src/main/java/com/practica/proyectito/ProyectitoApplication.java
@@ -2,12 +2,17 @@ package com.practica.proyectito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProyectitoApplication {
+public class ProyectitoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectitoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectitoApplication.class, args);
 	}
-
 }
---
+++ proyectito/src/main/webapp/WEB-INFO/index.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Proyecto</title>
+</head>
+<body>
+    <h1>Bienvenido a mi aplicación</h1>
+
+    <!-- Botón para redirigir a la página de creación de productos -->
+    <a href="${pageContext.request.contextPath}/productos/new">
+        <button type="button">Crear Nuevo Producto</button>
+    </a>
+</body>
+</html>
